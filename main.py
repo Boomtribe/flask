c4a4731a9e11812b2ac26019786f7848005e7f5f
Boom Tribe Records <boomtribe.blockchain@ud.me>
2024-10-11 05:08:16 +0000

    first

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+socketio = SocketIO(app)
 
-
+# Startseite
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return render_template('index.html')  # Stelle sicher, dass du diese Datei erstellst
 
+# Nachrichten-Handling über WebSockets
+@socketio.on('message')
+def handle_message(msg):
+    print(f"Message: {msg}")
+    send(msg, broadcast=True)  # Nachricht an alle verbundene Clients senden
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    socketio.run(app)
